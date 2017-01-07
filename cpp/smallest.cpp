7933c027e5f2d1d01ec0203e54ae9af2f1818fde
rule0x42 <rule0x42@gmail.com>
2017-01-07 01:02:34 -0500

    This is my second commit

--- cpp/smallest.cpp
+++ cpp/smallest.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main(void)
+void cabbage(int);
+
+int main()
 {
+    using namespace std;
+
+    int n;
+
+    cout << "Hello, what is your age? ";
+    cin >> n;
+    cabbage(n);
+
+    return 0;
+}
+
+void cabbage(int n)
+{
+    using namespace std;
+
+    cout << "You were born " << n << " year(s) ago..." << endl;
 }
